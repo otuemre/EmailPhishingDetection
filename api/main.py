01a4ba9f8b3cab4be821990402c2dfed6f0f161a
otuemre <dev.emre17@gmail.com>
2025-05-28 14:10:24 +0100

    Add custom 404 page and handler

--- api/main.py
+++ api/main.py
@@ -3,18 +3,26 @@ from api.pipeline import predict_email
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.requests import Request
+from starlette.responses import Response
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://phishingdetection.net"], # Make it ['*'] for local testing
+    allow_origins=["https://phishingdetection.net"],  # Make it ['*'] for local testing
     allow_credentials=True,
     allow_methods=["POST", "GET"],
     allow_headers=["*"],
 )
 
 app.mount('/static', StaticFiles(directory='frontend/static'), name='static')
+
+
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+    return FileResponse("frontend/static/404.html", status_code=404)
 
 
 @app.get('/')
